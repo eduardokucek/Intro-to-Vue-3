945dd81a16bb7ba54ac2a1d02f98c6b7a0701154
eduardokucek <eduardo.kucek@gmail.com>
2023-11-10 10:59:03 -0300

    list

--- main.js
+++ main.js
@@ -4,9 +4,21 @@ const app = Vue.createApp({
       product: "Socks",
       description: "This socks rules!",
       image: "./assets/images/socks_green.jpg",
-      //   inStock: true,
-      inventory: 0,
-      onSale: true,
+      inStock: true,
+      inventory: 100,
+      onSale: false,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+        },
+        {
+          id: 2235,
+          color: "blue",
+        },
+      ],
+      sizes: ["35/36", "37/38", "39/40", "41/42"],
     };
   },
 });
