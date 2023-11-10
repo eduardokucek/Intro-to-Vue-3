277814fdf6deae59a484da23f5bbe415ce6ad470
eduardokucek <eduardo.kucek@gmail.com>
2023-11-10 11:43:10 -0300

    add/remove from cart

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
       description: "This socks rules!",
       image: "./assets/images/socks_green.jpg",
@@ -12,13 +13,30 @@ const app = Vue.createApp({
         {
           id: 2234,
           color: "green",
+          image: "./assets/images/socks_green.jpg",
         },
         {
           id: 2235,
           color: "blue",
+          image: "./assets/images/socks_blue.jpg",
         },
       ],
       sizes: ["35/36", "37/38", "39/40", "41/42"],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart <= 0) {
+        this.cart;
+      } else {
+        this.cart -= 1;
+      }
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
